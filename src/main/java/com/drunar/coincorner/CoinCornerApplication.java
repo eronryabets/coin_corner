fb184e124f58cbb6c36ab0002f22bd5ff1725aa2
eronryabets <eronryabets@gmail.com>
2023-08-06 11:18:30 +0300

    first commit

---
+++ src/main/java/com/drunar/coincorner/CoinCornerApplication.java
@@ -0,0 +1,13 @@
+package com.drunar.coincorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinCornerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinCornerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/drunar/coincorner/CoinCornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drunar.coincorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinCornerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
