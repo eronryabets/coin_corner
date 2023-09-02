76729bbeb318b56d87d8c9f0293646cd4c906629
eronryabets <eronryabets@gmail.com>
2023-09-02 14:13:10 +0300

    Added a separate page output for Access Denied in ControllerExceptionHandler.

--- src/main/java/com/drunar/coincorner/http/handler/ControllerExceptionHandler.java
+++ src/main/java/com/drunar/coincorner/http/handler/ControllerExceptionHandler.java
@@ -1,16 +1,22 @@
 package com.drunar.coincorner.http.handler;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
 @ControllerAdvice(basePackages = "com.drunar.coincorner.http.controller")
-public class ControllerExceptionHandler /*extends ResponseEntityExceptionHandler*/ {
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public String handleAccessDeniedException(AccessDeniedException exception) {
+        log.error("Access Denied", exception);
+        return "error/accessDenied";
+    }
 
     @ExceptionHandler(Exception.class)
-    public String handleExceptions(Exception exception, HttpServletRequest request) {
+    public String handleOtherExceptions(Exception exception) {
         log.error("Failed to return response", exception);
         return "error/error";
     }
